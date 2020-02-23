a903be47f9ff23f3b34084b78b97a947d28fef42
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-02-23 17:07:23 -0300

    :sparkles: adicionando comentários para teste

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -22,7 +22,43 @@ class PostList extends Component {
               name: "Diego Fernandes",
               avatar: "https://i.imgur.com/Du3DCpG.png"
             },
-            content: "A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real)"
+            content: "A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real)",
+            comments: [
+              {
+                id: 1,
+                author: {
+                  name: "Julio Alcantara",
+                  avatar: "https://i.imgur.com/Cczah9q.png"
+                },
+                content: "Lorem ipsum dolor sit amet",
+                comments: [
+                  {
+                    id: 1,
+                    author: {
+                      name: "Gabriel Lisboa",
+                      avatar: "https://i.imgur.com/Ojf9dzz.png"
+                    },
+                    content: "Outro comentário",
+                  }
+                ]
+              },
+              {
+                id: 2,
+                author: {
+                  name: "Clara Lisboa",
+                  avatar: "https://i.imgur.com/j7pBWuO.png"
+                },
+                content: "Outro comentário",
+              }
+            ],
+          },
+          {
+            id: 2,
+            author: {
+              name: "Gabriel Lisboa",
+              avatar: "https://i.imgur.com/Ojf9dzz.png"
+            },
+            content: "Mais um comentário",
           }
         ]
       },
