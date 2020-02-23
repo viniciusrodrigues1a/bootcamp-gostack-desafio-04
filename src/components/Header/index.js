4670a50131991d04c57a8956866d9340e14ecc27
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-02-23 10:32:00 -0300

    :sparkles: Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './styles.css';
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <span>facebook</span>
+        <button type="button">Meu perfil</button>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
